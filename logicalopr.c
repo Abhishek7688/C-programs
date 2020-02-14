7317117cf005de9f392244d0a04e27ca151b52d5
Abhishek Maheshwari <57298257+abhishek76888@users.noreply.github.com>
2020-02-15 02:35:54 +0530

    Add files via upload

---
+++ logicalopr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=34;
+    b=4;
+    
+    printf("a&&b=%d\n",a&&b);
+    printf("a||b=%d\n",a||b);
+    printf("a*b=%d\n",!b);
+    printf("a/b=%d\n",a<=b);
+    
+    return 0;
+}
