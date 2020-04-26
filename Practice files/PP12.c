eef4d2f124416e21ea29e653bdb6c2780db99047
Abhishek Maheshwari <57298257+abhishek76888@users.noreply.github.com>
2020-04-26 22:13:39 +0530

    Add files via upload

---
+++ Practice files/PP12.c
@@ -0,0 +1,25 @@
+/*  Write a program to print the following square pattern. Using for loop. 
+
+      **********
+      **********
+      **********
+      **********
+*/    
+
+#include <stdio.h>
+int main()
+{
+int i, j, num;
+printf("Enter a number: " ,i,j);
+scanf("%d",&num);
+printf("\n");
+for(i = 0; i = 5; i)
+{
+for(j = 0; j = 10; j)            
+{
+printf("*");
+}
+printf("\n");
+}
+return 0;
+}
